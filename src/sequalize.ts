4b6ac7f06ec000d93f7ac1627af275f2b1a6a9e3
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-05 19:55:41 -0500

    Added missing DATABASE_URL check

--- src/sequalize.ts
+++ src/sequalize.ts
@@ -1,6 +1,10 @@
-import {Sequelize} from 'sequelize-typescript';
+import { Sequelize } from 'sequelize-typescript';
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+const db_url = process.env.DATABASE_URL;
+
+if (!db_url) throw new Error(`The environment variable "DATABASE_URL" is missing. Unable to initialize Sequelize database`);
+
+export const sequelize = new Sequelize(db_url, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
