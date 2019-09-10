517eff64a9b3ab915f6df323f01af8e9deaaf48d
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-10 02:15:22 -0500

    Swapping POST and PUT to match standard

--- src/api/projects/post.ts
+++ src/api/projects/post.ts
@@ -1,13 +1,7 @@
 import { Request, Response } from "express";
 import User from "../../models/User"
 import Project from "../../models/Project";
-import { findSimilarProjectName } from '../../common/helpers';
 import { IProject } from "../../models/types";
-
-interface IProjectUpdateRequest {
-    oldProjectData: IProject;
-    newProjectData: IProject;
-}
 
 module.exports = (req: Request, res: Response) => {
     const body = req.body;
@@ -22,19 +16,19 @@ module.exports = (req: Request, res: Response) => {
         return;
     }
 
-    updateProject(body)
+    submitProject(body)
         .then(results => {
-            res.end("Success");
+            res.end(JSON.stringify(results))
         })
         .catch(err => {
             console.error(err);
             res.status(500);
             res.end(`Internal server error: ${err}`);
         });
-
 };
 
-function checkIProject(body: IProject): true | string {
+function checkBody(body: IProject): true | string {
+    if (!body.user) return "user";
     if (!body.user.name) return "user.name";
     if (!body.user.discordId) return "user.discordId";
 
@@ -45,37 +39,46 @@ function checkIProject(body: IProject): true | string {
     return true;
 }
 
-function checkBody(data: IProjectUpdateRequest): true | string {
-    if (data.oldProjectData == undefined) return "oldProjectData";
-    if (checkIProject(data.oldProjectData) !== true) return "oldProjectData." + checkIProject(data.oldProjectData);
+function submitProject(projectData: IProject): Promise<Project> {
+    return new Promise<Project>(async (resolve, reject) => {
 
-    if (data.newProjectData == undefined) return "newProjectData";
-    if (checkIProject(data.newProjectData) !== true) return "newProjectData." + checkIProject(data.newProjectData);
+        if (await checkForExistingProject(projectData).catch(reject)) {
+            reject("A project with that name already exists");
+            return;
+        }
 
-    return true;
-}
+        // Get a matching user
+        const user = await getUserFromDB(projectData.user.discordId);
+        if (!user) {
+            reject("User not found");
+            return;
+        }
 
-function updateProject(projectUpdateData: IProjectUpdateRequest): Promise<Project> {
-    return new Promise<Project>((resolve, reject) => {
+        // Set the userId to the found user
+        projectData.userId = user.id;
 
-        Project.findAll({
-            include: [{
-                model: User,
-                where: { discordId: projectUpdateData.oldProjectData.user.discordId }
-            }]
-        }).then(projects => {
-            if (projects.length === 0) { reject(`Projects with ID ${projectUpdateData.oldProjectData.user.discordId} not found`); return; }
-
-            // Filter out the correct app name
-            const project = projects.filter(project => JSON.parse(JSON.stringify(project)).appName == projectUpdateData.oldProjectData.appName);
-
-            let similarAppName = findSimilarProjectName(projects, projectUpdateData.oldProjectData.appName);
-            if (project.length === 0) { reject(`Project with name "${projectUpdateData.oldProjectData.appName}" could not be found. ${(similarAppName !== undefined ? `Did you mean ${similarAppName}?` : "")}`); return; }
-            if (project.length > 1) { reject("More than one project with that name found. Contact a system administrator to fix the data duplication"); return; }
-
-            project[0].update({ ...projectUpdateData.newProjectData })
-                .then(resolve)
-                .catch(reject);
-        }).catch(reject);
+        // Create the project
+        Project.create(
+            { ...projectData })
+            .then(resolve)
+            .catch(reject);
     });
 }
+
+function getUserFromDB(discordId: string): Promise<User | null> {
+    return new Promise(async (resolve, reject) => {
+        User.findOne({
+            where: { discordId: discordId }
+        }).then(resolve).catch(reject);
+    })
+}
+
+function checkForExistingProject(project: IProject): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+        Project.findAll({
+            where: { appName: project.appName }
+        }).then(projects => {
+            resolve(projects.length > 0);
+        }).catch(reject)
+    });
+} 
--- src/api/projects/put.ts
+++ src/api/projects/put.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
 import User from "../../models/User"
 import Project from "../../models/Project";
+import { findSimilarProjectName } from '../../common/helpers';
 import { IProject } from "../../models/types";
+
+interface IProjectUpdateRequest {
+    oldProjectData: IProject;
+    newProjectData: IProject;
+}
 
 module.exports = (req: Request, res: Response) => {
     const body = req.body;
@@ -16,19 +22,19 @@ module.exports = (req: Request, res: Response) => {
         return;
     }
 
-    submitProject(body)
+    updateProject(body)
         .then(results => {
-            res.end(JSON.stringify(results))
+            res.end("Success");
         })
         .catch(err => {
             console.error(err);
             res.status(500);
             res.end(`Internal server error: ${err}`);
         });
+
 };
 
-function checkBody(body: IProject): true | string {
-    if (!body.user) return "user";
+function checkIProject(body: IProject): true | string {
     if (!body.user.name) return "user.name";
     if (!body.user.discordId) return "user.discordId";
 
@@ -39,46 +45,37 @@ function checkBody(body: IProject): true | string {
     return true;
 }
 
-function submitProject(projectData: IProject): Promise<Project> {
-    return new Promise<Project>(async (resolve, reject) => {
+function checkBody(data: IProjectUpdateRequest): true | string {
+    if (data.oldProjectData == undefined) return "oldProjectData";
+    if (checkIProject(data.oldProjectData) !== true) return "oldProjectData." + checkIProject(data.oldProjectData);
 
-        if (await checkForExistingProject(projectData).catch(reject)) {
-            reject("A project with that name already exists");
-            return;
-        }
+    if (data.newProjectData == undefined) return "newProjectData";
+    if (checkIProject(data.newProjectData) !== true) return "newProjectData." + checkIProject(data.newProjectData);
 
-        // Get a matching user
-        const user = await getUserFromDB(projectData.user.discordId);
-        if (!user) {
-            reject("User not found");
-            return;
-        }
-
-        // Set the userId to the found user
-        projectData.userId = user.id;
-
-        // Create the project
-        Project.create(
-            { ...projectData })
-            .then(resolve)
-            .catch(reject);
-    });
+    return true;
 }
 
-function getUserFromDB(discordId: string): Promise<User | null> {
-    return new Promise(async (resolve, reject) => {
-        User.findOne({
-            where: { discordId: discordId }
-        }).then(resolve).catch(reject);
-    })
-}
+function updateProject(projectUpdateData: IProjectUpdateRequest): Promise<Project> {
+    return new Promise<Project>((resolve, reject) => {
 
-function checkForExistingProject(project: IProject): Promise<boolean> {
-    return new Promise<boolean>((resolve, reject) => {
         Project.findAll({
-            where: { appName: project.appName }
+            include: [{
+                model: User,
+                where: { discordId: projectUpdateData.oldProjectData.user.discordId }
+            }]
         }).then(projects => {
-            resolve(projects.length > 0);
-        }).catch(reject)
+            if (projects.length === 0) { reject(`Projects with ID ${projectUpdateData.oldProjectData.user.discordId} not found`); return; }
+
+            // Filter out the correct app name
+            const project = projects.filter(project => JSON.parse(JSON.stringify(project)).appName == projectUpdateData.oldProjectData.appName);
+
+            let similarAppName = findSimilarProjectName(projects, projectUpdateData.oldProjectData.appName);
+            if (project.length === 0) { reject(`Project with name "${projectUpdateData.oldProjectData.appName}" could not be found. ${(similarAppName !== undefined ? `Did you mean ${similarAppName}?` : "")}`); return; }
+            if (project.length > 1) { reject("More than one project with that name found. Contact a system administrator to fix the data duplication"); return; }
+
+            project[0].update({ ...projectUpdateData.newProjectData })
+                .then(resolve)
+                .catch(reject);
+        }).catch(reject);
     });
-} 
+}
