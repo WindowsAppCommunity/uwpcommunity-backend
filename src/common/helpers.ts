ce208c7e83f97ef8a3c6fe830f2140b9cfa0823f
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-09 22:02:54 -0500

    Added levenshteinDistance helper

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -18,6 +18,38 @@ function remove(text: string, target: string) {
     return text.split(target).join("");
 };
 
+/***
+ * @summary Comput the edit distance between two given strings
+ * @see https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#JavaScript
+ */
+export function levenshteinDistance(a: string, b: string) {
+    if (a.length === 0) return b.length;
+    if (b.length === 0) return a.length;
+    var matrix = [];
+    var i;
+    for (i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    var j;
+    for (j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    for (i = 1; i <= b.length; i++) {
+        for (j = 1; j <= a.length; j++) {
+            if (b.charAt(i - 1) == a.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i - 1][j - 1] + 1, // substitution
+                    Math.min(matrix[i][j - 1] + 1, // insertion
+                        matrix[i - 1][j] + 1)); // deletion
+            }
+        }
+    }
+
+    return matrix[b.length][a.length];
+};
 module.exports = {
-    match, replaceAll, remove
+    match, replaceAll, remove, levenshteinDistance
 };
