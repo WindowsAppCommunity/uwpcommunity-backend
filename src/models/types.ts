962c56b7fde3a6f83714bc20cb9ec8bd266d7cb1
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-10 00:49:51 -0500

    Add note about public emails

--- src/models/types.ts
+++ src/models/types.ts
@@ -9,5 +9,5 @@ export interface IProject {
 export interface IUser {
     name: string;
     discordId: string;
-    email?: string;
+    email?: string; // This is a contact email supplied by the user, and is safe to be public 
 }
