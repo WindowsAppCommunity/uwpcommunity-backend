b8e04e3fa8f2afa4bbf12bc81a963137fc361a0c
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-10 00:45:02 -0500

    Add PUT user (user creation)

---
+++ src/api/user/put.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import User from "../../models/User"
+import { IUser } from "../../models/types";
+
+module.exports = (req: Request, res: Response) => {
+    const body = req.body;
+    const bodyCheck = checkBody(body);
+
+    if (bodyCheck !== true) {
+        res.status(422);
+        res.json(JSON.stringify({
+            error: "Malformed request",
+            reason: `Parameter "${bodyCheck}" not provided or malformed`
+        }));
+        return;
+    }
+
+    submitProject(body)
+        .then(results => {
+            res.end(JSON.stringify(results))
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500);
+            res.end(`Internal server error: ${err}`);
+        });
+
+};
+
+function checkBody(body: IUser): true | string {
+    if (!body.name) return "name";
+    if (!body.discordId) return "discordId";
+    return true;
+}
+
+function submitProject(userData: IUser): Promise<User> {
+    return new Promise<User>((resolve, reject) => {
+        User.create(
+            { ...userData },
+            {
+                include: [User]
+            })
+            .then(resolve)
+            .catch(reject);
+    });
+}
+
