70520dc10861bc1e5fec299396e5e47d908da786
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-09 23:26:57 -0500

    Return Success instead of giving back project data

--- src/api/projects/post.ts
+++ src/api/projects/post.ts
@@ -36,7 +36,7 @@ module.exports = (req: Request, res: Response) => {
 
     updateProject(body)
         .then(results => {
-            res.end(JSON.stringify(results))
+            res.end("Success")
         })
         .catch(err => {
             console.error(err);
