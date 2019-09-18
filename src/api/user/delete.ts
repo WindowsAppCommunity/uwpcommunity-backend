aba0278beed72e3355a4456a1819b2221e96ef48
rymate1234 <ryan.murthick@gmail.com>
2019-09-18 23:12:36 +0100

    Remove invalid DELETE body from user API
    (swagger complained)

--- src/api/user/delete.ts
+++ src/api/user/delete.ts
@@ -1,12 +1,8 @@
 import { Request, Response } from "express";
-import User from "../../models/User"
-import { IProject, IUser, IDiscordUser } from "../../models/types";
-import Project from "../../models/Project";
+import { IDiscordUser } from "../../models/types";
 import { getUserByDiscordId, getProjectsByUserDiscordId, GetDiscordUser, genericServerError } from "../../common/helpers";
 
 module.exports = (req: Request, res: Response) => {
-    const body = req.body;
-
     if (req.query.accessToken == undefined) {
         res.status(422);
         res.json({
@@ -16,15 +12,6 @@ module.exports = (req: Request, res: Response) => {
         return;
     }
 
-    const bodyCheck = checkBody(body);
-    if (bodyCheck !== true) {
-        res.status(422);
-        res.json({
-            error: "Malformed request",
-            reason: `Parameter "${bodyCheck}" not provided or malformed`
-        });
-        return;
-    }
     (async () => {
         const user = await GetDiscordUser(req.query.accessToken).catch((err) => genericServerError(err, res));
         if (!user) {
@@ -50,11 +37,6 @@ module.exports = (req: Request, res: Response) => {
             .catch((err) => genericServerError(err, res));
     })();
 };
-
-function checkBody(body: IUser): true | string {
-    if (!body.name) return "name";
-    return true;
-}
 
 /**
  * @returns True if successful, false if user not found
