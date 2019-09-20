6b70d4333223760cafbe6f246f53522546d939fc
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-20 06:47:58 -0500

    Add authentication to GET user roles

--- src/api/bot/user/roles/get.ts
+++ src/api/bot/user/roles/get.ts
@@ -1,21 +1,28 @@
 import { Request, Response } from "express-serve-static-core";
 import { GetGuildUser } from "../../../../common/discord";
 import { Role } from "discord.js";
-import { genericServerError } from "../../../../common/helpers";
+import { genericServerError, GetDiscordUser } from "../../../../common/helpers";
 
 module.exports = async (req: Request, res: Response) => {
-    let discordId = req.query.discordId;
-
-    if (!discordId) {
+    if (!req.headers.authorization) {
         res.status(422);
         res.json(JSON.stringify({
             error: "Malformed request",
-            reason: "Missing discordId query"
+            reason: "Missing authorization header"
         }));
         return;
     }
 
-    const guildMember = await GetGuildUser(discordId);
+    let accessToken = req.headers.authorization.replace("Bearer ", "");
+
+    const user = await GetDiscordUser(accessToken).catch((err) => genericServerError(err, res));
+    if (!user) {
+        res.status(401);
+        res.end(`Invalid access token`);
+        return;
+    }
+
+    const guildMember = await GetGuildUser(user.id);
     if (!guildMember) {
         genericServerError("Unable to get guild details", res);
         return;
