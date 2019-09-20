a2d9196bc8976a3d150830e4c710eadc2c1fd2c7
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-20 05:02:48 -0500

    Added get user roles endpoint

---
+++ src/api/bot/user/roles/get.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express-serve-static-core";
+import { GetGuildUser } from "../../../../common/discord";
+import { Role } from "discord.js";
+import { genericServerError } from "../../../../common/helpers";
+
+module.exports = async (req: Request, res: Response) => {
+    let discordId = req.query.discordId;
+
+    if (!discordId) {
+        res.status(422);
+        res.json(JSON.stringify({
+            error: "Malformed request",
+            reason: "Missing discordId query"
+        }));
+        return;
+    }
+    
+    const guildMember = await GetGuildUser(discordId);
+    if (!guildMember) {
+        genericServerError("Unable to get guild details", res);
+        return;
+    }
+
+    let roles: Role[] = guildMember.roles.array().map(role => { delete role.guild; return role });
+
+    res.json(roles);
+};
