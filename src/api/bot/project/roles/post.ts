ad4a0ebe48c2bec0eaa109ff9c8967ae51c5d519
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-20 07:13:32 -0500

    Refactored & tested - Check that the role doesn't already exist

--- src/api/bot/project/roles/post.ts
+++ src/api/bot/project/roles/post.ts
@@ -27,10 +27,16 @@ module.exports = async (req: Request, res: Response) => {
         genericServerError("Unable to get guild details", res);
         return;
     }
-    // Must have a role in the body (JSON)
-    if (!req.body.name) {
+
+    // Must have a proper roles in the body (JSON)
+    if (!req.body.appName) {
         res.status(422);
-        res.end(`Missing role name`);
+        res.end(`Missing appName`);
+        return;
+    }
+    if (!req.body.subRole) {
+        res.status(422);
+        res.end(`Missing subRole`);
         return;
     }
 
@@ -38,35 +44,39 @@ module.exports = async (req: Request, res: Response) => {
 
     // If trying to create a role for a project, make sure the project exists
     let Projects = await getProjectsByUserDiscordId(user.id);
-    if (Projects.filter(project => req.body.name.includes(project.appName)).length == 0) {
+    if (Projects.filter(project => req.body.appName == project.appName).length == 0) {
         res.status(422);
-        res.end(`The project doesn't exist, or the role does not contain the project name`);
+        res.end(`The project doesn't exist`);
         return;
     }
 
-    if (!checkAllowedProjectSubRoles(req.body.name)) {
+    if (allowedProjectSubRoles.filter(subRole => req.body.subRole == subRole).length == 0) {
         res.status(422);
-        res.end(`Invalid project sub-role. Allowed values are: "${allowedProjectSubRoles.join(`" , "`)}"`);
+        res.end(`Invalid project subRole. Allowed values are: "${allowedProjectSubRoles.join(`" , "`)}"`);
         return;
     }
 
     const server = GetGuild();
     if (!server) return;
 
+    const roleName = req.body.appName + " " + capitalizeFirstLetter(req.body.subRole);
+    // Check that the role doesn't already exist
+    if (server.roles.array().filter(role => role.name == roleName).length > 0) {
+        res.status(401);
+        res.end("Role already exists");
+        return;
+    }
+
     server.createRole({
-        name: req.body.name,
+        name: req.body.appName + " " + capitalizeFirstLetter(req.body.subRole),
         mentionable: true,
         color: req.body.color
     });
 
-    res.json(roles);
+    res.end("Success");
 };
 
 const allowedProjectSubRoles = ["translator", "dev", "beta tester"];
-
-function checkAllowedProjectSubRoles(roleName: string) {
-    for (let SubRole in allowedProjectSubRoles) {
-        if (roleName.toLowerCase().includes(SubRole)) return true;
-    }
-    return false;
+function capitalizeFirstLetter(s: string) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
 }
