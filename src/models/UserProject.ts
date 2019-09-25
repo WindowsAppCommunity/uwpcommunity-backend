8a5c179e04041757aff622c381c083ad626e9ebe
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-25 06:05:51 -0500

    Added UserOwnsProject helper

--- src/models/UserProject.ts
+++ src/models/UserProject.ts
@@ -41,6 +41,12 @@ export async function GetUsersByProjectId(ProjectId: number) {
     return users;
 }
 
+export async function UserOwnsProject(user: User, project: Project): Promise<boolean> {
+    const OwnedUserProject = await UserProject.findAll({ where: { isOwner: true, userId: user.id, projectId: project.id } });
+    if (OwnedUserProject.length > 0) return true;
+    return false;
+}
+
 export async function GetProjectsByUserId(UserId: number) {
     const RelevantUserProjects = await UserProject.findAll({ where: { userId: UserId } });
 
