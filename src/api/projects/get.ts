bd75419cce261cc391ea6068c14e89e057909a80
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-25 03:31:42 -0500

    Removed broken cache code from GET /projects/

--- src/api/projects/get.ts
+++ src/api/projects/get.ts
@@ -7,14 +7,14 @@ import { IProject } from "../../models/types";
 import { genericServerError } from "../../common/helpers/generic";
 
 module.exports = (req: Request, res: Response) => {
-    getProjectsCached(req.query.token)
+    getProjects(req.query.token)
         .then(result => {
             res.json(result);
         })
         .catch(err => genericServerError(err, res));
 };
 
-export function getProjects(token?: string, shouldCache = true): Promise<IProject[]> {
+export function getProjects(token?: string): Promise<IProject[]> {
     return new Promise((resolve, reject) => {
         Project
             .findAll((token ? {
@@ -29,50 +29,12 @@ export function getProjects(token?: string, shouldCache = true): Promise<IProjec
 
                     for (let project of results) {
                         let proj = await DbToStdModal_Project(project).catch(reject);
-                        console.log(proj);
                         if (proj) projects.push(proj);
                     }
 
                     resolve(projects);
-                    if (shouldCache) fs.writeFile(launchTableCachePath, JSON.stringify(projects), () => { }); // Cache the results
                 }
             })
             .catch(reject);
-    });
-}
-
-
-// Get and cache the list of launch participants
-// This API is our only surface for interacting with the database, so the cache should be updated when a new participant is added
-const fs = require("fs");
-const launchTableCacheFilename: string = "launchTableCache.json";
-const launchTableCachePath = path.join(__dirname, launchTableCacheFilename);
-
-export function getProjectsCached(token?: string): Promise<IProject[]> {
-    return new Promise((resolve, reject) => {
-
-        fs.readdir(__dirname, (err: Error, fileResults: string[] | Buffer[] | Dirent) => {
-            // If missing, get data from database and create the cache
-            if (!(fileResults instanceof Array && fileResults instanceof String) || !fileResults.includes(launchTableCacheFilename)) {
-                console.info("Data not cached, refreshing from DB");
-                getProjects(token)
-                    .then(resolve)
-                    .catch(reject)
-                return;
-            }
-
-            // If the file exists, get the contents
-            fs.readFile(launchTableCachePath, (err: Error, file: string[] | Buffer[] | Dirent) => {
-                let fileContents = file.toString();
-
-                if (fileContents.length <= 5) {
-                    // Retry
-                    getProjectsCached(token);
-                    return;
-                }
-                resolve(JSON.parse(fileContents));
-            });
-        });
-
     });
 }
