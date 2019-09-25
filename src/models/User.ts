8b68657c55c7d1e6df0f005c843f43ee1116e9cd
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-09-25 00:53:38 -0500

    Added converter for Db to Standard user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,10 @@
 import { Column, CreatedAt, Model, Table, UpdatedAt, PrimaryKey, AutoIncrement, DataType, BelongsToMany } from 'sequelize-typescript';
-import Project  from './Project';
+import Project, { DbToStdModal_Project } from './Project';
 import * as faker from 'faker'
 
 import UserProject from "./UserProject";
-  
+import { IUser, IProject } from './types';
+
 @Table
 export default class User extends Model<User> {
 
@@ -22,18 +23,39 @@ export default class User extends Model<User> {
     @Column
     discordId!: string;
 
-    
+
     @BelongsToMany(() => Project, () => UserProject)
     projects?: Project[];
-    
-    
+
+
     @CreatedAt
     @Column
     createdAt!: Date;
-    
+
     @UpdatedAt
     @Column
     updatedAt!: Date;
+}
+
+export async function DbToStdModal_User(user: User): Promise<IUser | undefined> {
+    let projects: IProject[] = [];
+
+    // Convert db user projects to standard API models 
+    if (user.projects) {
+        for (let project of user.projects) {
+            let projectStd = await DbToStdModal_Project(project);
+            if (projectStd) projects.push(projectStd);
+        }
+    }
+
+    const stdUser: IUser = {
+        discordId: user.discordId,
+        name: user.name,
+        id: user.id,
+        email: user.email,
+        projects: projects
+    };
+    return stdUser;
 }
 
 export function GenerateMockUser(): User {
