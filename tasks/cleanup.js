2136704e6bf391e9af5df007c71b59bf06d5f2db
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-05 00:55:52 -0500

    Fixed crash when working with a clean directory

--- tasks/cleanup.js
+++ tasks/cleanup.js
@@ -2,10 +2,10 @@ const pathsToClean = ["./build"];
 const fs = require('fs-extra')
 
 for (let path of pathsToClean) {
-    console.log(`Cleaning up "${path}"`);
-    if (!fs.pathExistsSync(path)) throw new Error("Path does not exist");
-
-    fs.removeSync(path);
+    if (fs.pathExistsSync(path)) {
+        console.log(`Cleaning up "${path}"`);
+        fs.removeSync(path);
+    }
 }
 
 if (pathsToClean.length > 0) console.log(`Done`);
