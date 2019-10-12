570ec34135c5d100fd2eb5d5816ed09f578d8a0b
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-12 16:27:46 -0500

    Added query to return all projects unfiltered if user if a mod

--- src/api/projects/get.ts
+++ src/api/projects/get.ts
@@ -3,15 +3,32 @@ import User from "../../models/User";
 import Project, { DbToStdModal_Project } from "../../models/Project";
 import { IProject } from "../../models/types";
 import { genericServerError, validateAuthenticationHeader } from "../../common/helpers/generic";
-import { GetDiscordIdFromToken } from "../../common/helpers/discord";
-import { HttpStatus, BuildResponse } from "../../common/helpers/responseHelper";
+import { GetDiscordIdFromToken, GetGuildUser } from "../../common/helpers/discord";
+import { HttpStatus, BuildResponse, ResponsePromiseReject, IRequestPromiseReject } from "../../common/helpers/responseHelper";
 
 module.exports = async (req: Request, res: Response) => {
-    const results = await getAllProjects().catch(err => genericServerError(err, res));
-    if (results) BuildResponse(res, HttpStatus.Success, results);
+    const reqQuery = req.query as IGetProjectsRequestQuery;
+
+    const projects = await getAllProjects(reqQuery.all && await isMod(req, res)).catch((err: IRequestPromiseReject) => BuildResponse(res, err.status, err.reason));
+    if (projects) {
+        BuildResponse(res, HttpStatus.Success, projects);
+    }
 };
 
-export function getAllProjects(): Promise<IProject[]> {
+async function isMod(req: Request, res: Response): Promise<boolean> {
+    const authAccess = validateAuthenticationHeader(req, res, false);
+    if (authAccess) {
+        const discordId = await GetDiscordIdFromToken(authAccess, res, false);
+        if (!discordId) return false;
+
+        const user = await GetGuildUser(discordId);
+        if (!user) return false;
+        return user.roles.array().filter(role => role.name == "Mod" || role.name == "Admin").length > 0;
+    }
+    return false;
+}
+
+export function getAllProjects(all?: boolean): Promise<IProject[]> {
     return new Promise((resolve, reject) => {
         Project.findAll()
             .then(async results => {
@@ -21,15 +38,15 @@ export function getAllProjects(): Promise<IProject[]> {
                     for (let project of results) {
                         let proj = await DbToStdModal_Project(project).catch(reject);
                         // Only push a project if not private
-                        if (proj && !proj.isPrivate && !proj.needsManualReview) projects.push(proj);
+                        if (proj && (!proj.isPrivate && !proj.needsManualReview || all)) projects.push(proj);
                     }
 
                     resolve(projects);
                 }
-            })
-            .catch(reject);
+            }).catch(err => ResponsePromiseReject("Internal server error: " + err, HttpStatus.InternalServerError, reject));
     });
 }
 interface IGetProjectsRequestQuery {
-    discordId?: string;
+    /** @summary Only useable if user is a mod or admin */
+    all?: boolean;
 }
