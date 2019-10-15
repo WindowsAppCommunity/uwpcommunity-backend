19a499e8ff59fff25140eefc979878b16a01a299
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-10-14 23:28:03 -0500

    Mild cleanup

--- src/api/bot/user/roles/get.ts
+++ src/api/bot/user/roles/get.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express-serve-static-core";
 import { GetGuildUser, GetDiscordUser } from "../../../../common/helpers/discord";
-import { Role } from "discord.js";
 import { genericServerError, validateAuthenticationHeader, DEVENV } from "../../../../common/helpers/generic";
 import { BuildResponse, HttpStatus, } from "../../../../common/helpers/responseHelper";
 
@@ -13,15 +12,13 @@ module.exports = async (req: Request, res: Response) => {
         BuildResponse(res, HttpStatus.Unauthorized, "Invalid accessToken");
         return;
     }
-
-    let roles: Role[] = [];
-
+    
     const guildMember = await GetGuildUser(user.id);
     if (!guildMember) {
         genericServerError("Unable to get guild details", res);
         return;
     }
 
-    roles = guildMember.roles.array().map(role => { delete role.guild; return role });
+    let roles = guildMember.roles.array().map(role => { delete role.guild; return role });
     BuildResponse(res, HttpStatus.Success, roles);
 };
