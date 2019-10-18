0aaba25a20106e50d2efd1897f7a276b1ce806cd
Emiliano Magliocca <emiliano.magliocca@outlook.com>
2019-10-18 09:20:51 +0400

    - fixed typo - fixed build responser

--- src/api/projects/{id}/get.ts
+++ src/api/projects/{id}/get.ts
@@ -4,7 +4,7 @@ import Project, { DbToStdModal_Project } from "../../../models/Project";
 import { IProject } from "../../../models/types";
 import { genericServerError, validateAuthenticationHeader } from "../../../common/helpers/generic";
 import { GetDiscordIdFromToken } from "../../../common/helpers/discord";
-import { HttpStatus, BuildResponse, ResponsePromiseReject } from "../../../common/helpers/responseHelper";
+import { HttpStatus, BuildResponse, ResponsePromiseReject, IRequestPromiseReject } from "../../../common/helpers/responseHelper";
 
 module.exports = async (req: Request, res: Response) => {
     let id = req.params['id'];
@@ -14,52 +14,55 @@ module.exports = async (req: Request, res: Response) => {
     if (!authAccess) return;
 
     const authenticatedDiscordId = await GetDiscordIdFromToken(authAccess, res);
-
     if (authenticatedDiscordId) {
-        const result = await getPrjectById(id).catch(err => genericServerError(err, res));
 
-        if (result) {
-            if (result.isPrivate) {
-
-                let showPrivate = false;
-
-                if (result.collaborators) {
-                    result.collaborators.forEach(collaborator => {
-                        if (collaborator.discordId == authenticatedDiscordId) {
-                            showPrivate = true;
+        getProjectById(id)
+            .then(result => {
+                if (result) {
+                    if (result.isPrivate) {
+        
+                        let showPrivate = false;
+        
+                        if (result.collaborators) {
+                            result.collaborators.forEach(collaborator => {
+                                if (collaborator.discordId == authenticatedDiscordId) {
+                                    showPrivate = true;
+                                }
+                            });
                         }
-                    });
+        
+                        if (showPrivate) {
+                            BuildResponse(res, HttpStatus.Success, result);
+                        } else {
+                            BuildResponse(res, HttpStatus.Success, "");
+                        }
+        
+                    } else {
+                        BuildResponse(res, HttpStatus.Success, result);
+                    }
                 }
+            })
+            .catch((err: IRequestPromiseReject) => BuildResponse(res, err.status, err.reason));
 
-                if (showPrivate) {
-                    BuildResponse(res, HttpStatus.Success, result);
-                } else {
-                    BuildResponse(res, HttpStatus.Success, "");
-                }
-
-            } else {
-                BuildResponse(res, HttpStatus.Success, result);
-            }
-        }
     }
-
 };
 
-export function getPrjectById(projectId: string): Promise<IProject> {
+export function getProjectById(projectId: string): Promise<IProject> {
     return new Promise(async (resolve, reject) => {
 
         let project: IProject;
-        Project.findByPk(projectId, { include: [{ model: User }] }).then(
-            async result => {
-                if (result) {
-                    let proj = await DbToStdModal_Project(result).catch(reject);
-                    if(proj){
-                        project = proj;
-                        resolve(project);
+        Project.findByPk(projectId, { include: [{ model: User }] })
+            .then(
+                async result => {
+                    if (result) {
+                        let proj = await DbToStdModal_Project(result).catch(reject);
+                        if(proj){
+                            project = proj;
+                            resolve(project);
+                        }
                     }
                 }
-            }
-        ).catch(err => ResponsePromiseReject("Internal server error: " + err, HttpStatus.InternalServerError, reject));
+            ).catch(err => ResponsePromiseReject("Internal server error: " + err, HttpStatus.InternalServerError, reject));
 
     });
 }
