82b8ca1a3c57b086bb62b58fe2c1049116efd1d3
Arlo Godfrey <arlo.godfrey@outlook.com>
2020-05-15 14:58:01 -0500

    Added role info command

---
+++ src/bot/commands/roleinfo.ts
@@ -0,0 +1,26 @@
+import { Message } from "discord.js";
+import { GetGuild } from "../../common/helpers/discord";
+import { capitalizeFirstLetter } from "../../common/helpers/generic";
+
+export default async (discordMessage: Message, args: string[]) => {
+    const message = discordMessage.content.toLowerCase();
+
+    const server = GetGuild();
+    if (!server) return;
+
+    const roles = server.roles.array();
+    const role = roles.find(i => i.name.toLowerCase() == message);
+
+    if (!role) return;
+
+    const numberOfMembers = role.members.size;
+    const dateRoleCreated = role.createdAt.toUTCString();
+    const mentionable = role.mentionable;
+
+    await discordMessage.channel.sendMessage(
+        `__**${capitalizeFirstLetter(discordMessage.content)}\ role info**__:
+Member count: ${numberOfMembers}
+Date created: ${dateRoleCreated}
+Mentionable: ${mentionable}`);
+
+};
