58b8260a0564943a584b3c3cb791efcaac0846ac
Arlo Godfrey <arlo.godfrey@outlook.com>
2020-05-16 16:16:01 -0500

    Moved message handlers into separate files, added possible ban handler

--- src/bot/events/message.ts
+++ src/bot/events/message.ts
@@ -1,38 +1,8 @@
-import { Message, DMChannel, TextChannel } from "discord.js";
-import { GetGuild } from "../../common/helpers/discord";
-
-const swearRegex: RegExp = new RegExp(/fuck|\sass|dick|shit|pussy|cunt|whore|bastard|bitch|faggot/);
+import { Message } from "discord.js";
+import { handlePossibleBan } from "./messageHandlers/possibleBan";
+import { handleSwearFilter } from "./messageHandlers/swearFilter";
 
 export default (discordMessage: Message) => {
     handleSwearFilter(discordMessage);
-
-}
-
-async function handleSwearFilter(discordMessage: Message) {
-    const message = discordMessage.content.toLowerCase();
-
-    if (message.match(swearRegex)) {
-        const sentFromChannel = discordMessage.channel as TextChannel;
-        const channelPermsForAll = sentFromChannel.permissionsFor(discordMessage.guild.defaultRole);
-
-
-        // If the channel is private, don't filter
-        if (channelPermsForAll && !channelPermsForAll.hasPermission("VIEW_CHANNEL")) {
-            return;
-        }
-        
-        const dm: DMChannel = await discordMessage.author.createDM();
-        await dm.sendMessage(`Your message was removed because it contained a swear word.
-> ${discordMessage.content}`);
-
-        const guild = GetGuild();
-        const author = discordMessage.author;
-        if (guild) {
-            const botChannel = guild.channels.find(i => i.name == "bot-stuff") as TextChannel;
-            botChannel.sendMessage(`A swear word from \`${author.username}#${author.discriminator}\` (ID ${author.id}) sent in <#${sentFromChannel.id}> was removed:
-> ${message}`);
-        }
-
-        await discordMessage.delete();
-    }
+    handlePossibleBan(discordMessage);
 }
