5b401af6a34d88a97ce5e5ef9dc904671ac8e8ac
Arlo Godfrey <arlo.godfrey@outlook.com>
2021-05-02 13:23:10 +0000

    Remove uneeded authenticate when getting project by tags

--- src/api/projects/tags/get.ts
+++ src/api/projects/tags/get.ts
@@ -1,13 +1,8 @@
 import { Request, Response } from "express";
-import { validateAuthenticationHeader } from "../../../common/helpers/generic";
 import { DbToStdModal_Project, getAllDbProjects } from "../../../models/Project";
 
 module.exports = async (req: Request, res: Response) => {
     const reqQuery = req.query as IGetProjectTagRequestQuery;
-
-    // If someone wants the projects for a specific user, they must be authorized
-    const authAccess = validateAuthenticationHeader(req, res);
-    if (!authAccess) return;
 
     const queryValidation = checkQuery(reqQuery);
     if (queryValidation !== true) {
