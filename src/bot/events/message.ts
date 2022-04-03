6a895c90fd2311a8bf1d4cd4b9429ffb62befdd8
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-04-03 18:23:36 -0500

    Fixed issue where bot would respond to its own messages

--- src/bot/events/message.ts
+++ src/bot/events/message.ts
@@ -4,6 +4,9 @@ import { handlePossibleBan } from "./messageHandlers/possibleBan";
 import { handleSwearFilter } from "./messageHandlers/swearFilter";
 
 export default (discordMessage: Message) => {
+    if (discordMessage.author?.bot)
+        return; // ignore messages sent by bots.
+
     handleSwearFilter(discordMessage);
     handlePossibleBan(discordMessage);
     devChatterWarning(discordMessage);
